899b1bf7d66e1359935298349e66daea5db012f3
JurseyTANG <92135036+JurseyTANG@users.noreply.github.com>
2022-09-18 15:33:19 +0800

    made some change

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,11 @@
 public class Library {
+    public static void main(String arg[]){
+        Library lib = new Library();
+        if(lib.someLibraryMethod())
+        System.out.println("Hello, World!");
+    }
+
+    public boolean someLibraryMethod(){
+        return true;
+    }
 }
